6049037756d63a7bd8280b80aca28f804476f4a4
Gil Grady <gilchristgrady25@gmail.com>
2021-11-02 11:57:01 -0500

    Created basic character controller

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// The name has to have "My" on the beginning or Unity complains
+public class MyCharacterController : MonoBehaviour
+{
+    public float jumpForce = 3f;
+    public float speed = 1f;
+    public float maxSpeed = 1f;
+
+    public Transform leftFoot;
+    public Transform rightFoot;
+
+    public LayerMask floorMask;
+
+    private PlayerInput playerInput;
+    private Rigidbody2D rb;
+
+    private CharacterControls characterControls; // This is the generated json file
+
+    //Input Actions
+    private InputAction jumpAction;
+    private InputAction moveAction;
+
+    private void Awake()
+    {
+        characterControls = new CharacterControls();
+        
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
+
+        //Set the Input Actions
+        moveAction = characterControls.Controls.Move; // This is one way to set it
+        jumpAction = playerInput.actions["Jump"]; // This is a slightly different way that doesn't use the json file
+
+        //Subscribe to the actions
+        jumpAction.performed += Jump;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded())
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Update()
+    {
+        lateralMovement(); // For some reason, if this is done in FixedUpdate, input is ignored 99% of the time
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.Raycast(leftFoot.position, Vector2.down, 0.1f, floorMask)
+            || Physics2D.Raycast(rightFoot.position, Vector2.down, 0.1f, floorMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void lateralMovement()
+    {
+        if (isGrounded()) //Will only check for lateral movement while grounded. Experimental
+        {
+            Vector2 inputVector = moveAction.ReadValue<Vector2>();
+
+            if (Mathf.Abs(rb.velocity.x) < maxSpeed)
+            {
+                rb.AddForce(new Vector2(inputVector.x, 0) * speed, ForceMode2D.Force);
+            }
+            if (inputVector.Equals(Vector2.zero))
+            {
+                // Hits the character with a force exactly equal opposite to its X velocity
+                // This stops the 
+                rb.AddForce(new Vector2(-rb.velocity.x, 0));
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        characterControls.Controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        characterControls.Controls.Disable();
+    }
+}
