3a8f10bdfcffec006f89698302c57883f10670b2
Gilgantuan <49262170+GilchristG@users.noreply.github.com>
2023-05-31 13:51:08 -0500

    Deleted a bunch of stuff

--- Assets/Scripts/SpecificMoveManagers/SpecificMoveManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SpecificMoveManager : MonoBehaviour
-{
-    public MyCharacterController moveController;
-    public abstract void DoCoolStuff(int i);
-}
